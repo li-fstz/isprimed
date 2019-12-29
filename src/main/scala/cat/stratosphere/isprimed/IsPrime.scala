f7d2ad7a0da4b3837d6e0091aa24a9c830b75d7a
li-fstz <li-fstz@qq.com>
2019-12-30 01:58:52 +0800

    change format

--- src/main/scala/cat/stratosphere/isprimed/IsPrime.scala
+++ src/main/scala/cat/stratosphere/isprimed/IsPrime.scala
@@ -108,7 +108,7 @@ object IsPrime {
                             if (factors.length == 1) {
                                 ret (s"$num 是一个质数")
                             } else {
-                                ret (s"$num 不是一个质数\n$num = ${format (factors)}")
+                                ret (s"$num 不是一个质数\n$num = ${format2 (factors)}")
                             }
                         } else {
                             ret ("ERROR: Num Limit Exceeded.\nNOTICE: 2 ≤ x ≤ 1e15.")
@@ -136,7 +136,7 @@ object IsPrime {
                         if (factors.length == 1) {
                             ret (s"今天是一个质数，\n$today 是一个质数")
                         } else {
-                            ret (s"今天不是一个质数，\n$today = ${format (factors)}")
+                            ret (s"今天不是一个质数，\n$today = ${format2 (factors)}")
                         }
                     }
                     case "isPrime" => ret ("你可以发给我一个数 x\n(2 ≤ x ≤ 10^15)，\n我会帮你判断 x 是不是一个质数")
